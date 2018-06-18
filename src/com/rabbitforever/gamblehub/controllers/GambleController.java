9787afaacba2e2b509a397b627f45e7062e5ee39
12006766g <12006766g@connect.polyu.hk>
2018-06-18 11:12:47 +0800

    merge commit

--- src/com/rabbitforever/gamblehub/controllers/GambleController.java
+++ src/com/rabbitforever/gamblehub/controllers/GambleController.java
@@ -1,12 +1,32 @@
 package com.rabbitforever.gamblehub.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import com.rabbitforever.gamblehub.services.UserService;
+import com.rabbitforever.gamblehub.models.eos.BigSmallEo;
+import com.rabbitforever.gamblehub.services.GambleService;
 
 @Controller
 public class GambleController {
     @Autowired
-    private UserService userService;
+    private GambleService gambleService;
+    
+    @PostMapping("/saveBigSmall")
+    public String saveBigSmallEo(@ModelAttribute("bigSmallEo") @Valid BigSmallEo bigSmallEo,
+                            BindingResult result, Model model) {
+ 
+//        if (result.hasErrors()) {
+//            model.addAttribute("bigSmallEoList", gambleService.read());
+//            return "editUsers";
+//        }
+ 
+        gambleService.save(bigSmallEo);
+        return "redirect:/";
+    }
 }
---
+++ src/com/rabbitforever/gamblehub/factories/DbUtilsFactory.java
@@ -0,0 +1,66 @@
+package com.rabbitforever.gamblehub.factories;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.rabbitforever.gamblehub.utils.DbUtils;
+import com.rabbitforever.gamblehub.utils.HibernateUtils;
+import com.rabbitforever.gamblehub.utils.MySqlDbUtils;
+public class DbUtilsFactory {
+	private final static Logger logger = LoggerFactory.getLogger(getClassName());
+
+	private static DbUtils mySqlDbUtils;
+	private static DbUtilsFactory dbUtilsFactory;
+//	private static DbUtils db2DbUtils;
+//	private static DbUtils msSqlDbUtils;
+	private static HibernateUtils hibernateUtils;
+	
+	private DbUtilsFactory(){
+		
+	}
+	public static DbUtilsFactory getInstanceOfDbUtilsFactory() {
+		if (dbUtilsFactory == null) {
+			dbUtilsFactory = new DbUtilsFactory();
+		}
+		return dbUtilsFactory;
+	}
+	public DbUtils getInstanceOfMySqlDbUtils() throws Exception{
+		try {
+			if (mySqlDbUtils == null){
+				mySqlDbUtils = new MySqlDbUtils();
+			}
+		} catch (Exception e) {
+			logger.error(getClassName() + ".getInstanceOfMySqlDbUtils()", e);
+			throw e;
+		}
+		return mySqlDbUtils;
+	}
+	public static String getClassName() {
+		String className = DbUtilsFactory.class.getName();
+		return className;
+	}
+	public HibernateUtils getInstanceOfHibernateUtils() throws Exception{
+		try {
+			if (hibernateUtils == null){
+				hibernateUtils = new HibernateUtils();
+			}
+		} catch (Exception e) {
+			logger.error(getClassName() + ".getInstanceOfHibernateUtils()", e);
+			throw e;
+		}
+		return hibernateUtils;
+	}
+//	public static DbUtils getInstanceOfDb2DbUtils() throws Exception{
+//		if (db2DbUtils == null){
+//			db2DbUtils = new Db2DbUtils();
+//		}
+//		return db2DbUtils;
+//	}
+//	
+//	public static DbUtils getInstanceOfMsSqlDbUtils() throws Exception{
+//		if (msSqlDbUtils == null){
+//			msSqlDbUtils = new MsSqlDbUtils();
+//		}
+//		return msSqlDbUtils;
+//	}
+}
